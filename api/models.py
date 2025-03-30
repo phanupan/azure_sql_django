cc54f0c01171638972122e0a3a0e50804c06e416
phanupan <81475304+phanupan@users.noreply.github.com>
2025-03-30 20:29:16 +0700

    Update models.py

--- api/models.py
+++ api/models.py
@@ -14,25 +14,6 @@ class Store(models.Model):
 class Product(models.Model):
     product_id = models.IntegerField(unique=True)
     product_name = models.CharField(max_length=100)
-    product_created_date = models.DateTimeField(auto_now_add=True)
-    product_updated_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.product_id} - {self.product_name}"
-
-class ProductList(generics.ListCreateAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-
-
-class ProductDetailUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    lookup_field = 'product_id'
-
-
-class ProductDeleteAll(generics.DestroyAPIView):
-    def delete(self, request, *args, **kwargs):
-        # Delete all products objects
-        Product.objects.all().delete()
-        return response.Response(status=status.HTTP_204_NO_CONTENT)
